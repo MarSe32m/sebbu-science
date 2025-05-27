e12861130094d700b39c0b3829ffb151928299df
Sebastian Toivonen <sebastian.toivonen@gmail.com>
2025-05-27 09:55:29 +0300

    Update WyRandomNumberGenerator

--- Sources/SebbuScience/Random/WyRandom.swift
+++ Sources/SebbuScience/Random/WyRandom.swift
@@ -9,14 +9,16 @@
 public struct WyRandomNumberGenerator: RandomNumberGenerator {
     @usableFromInline
     internal var state: UInt64
-
+    
+    @inlinable
     public init(seed: UInt64) {
         state = seed
     }
-
+    
+    @inlinable
     public mutating func next() -> UInt64 {
         state &+= 0xa0761d6478bd642f
-        let t = UInt128(state) &* UInt128(state ^ 0xe7037ed1a0b428db)
-        return UInt64((t >> 64) ^ t)
+        let mul = state.multipliedFullWidth(by: state ^ 0xe7037ed1a0b428db)
+        return mul.high ^ mul.low
     }
 }
